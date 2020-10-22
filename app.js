4c3589750fbbd45dacfedc01f5cc3171b2158972
varavk <varavk@gmail.com>
2020-10-22 23:32:25 +0530

    Demo chol

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use('/', express.static('public'));
+
+app.listen(process.env.PORT || '3000', () => {
+    console.log("server started")
+})
