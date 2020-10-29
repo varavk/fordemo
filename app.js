fc13e2b10ad3df3ae6b883266439bf1e0b3d003f
varavk <varavk@gmail.com>
2020-10-29 22:59:03 +0530

    prod mov 2

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(express.static('public'));
 
 app.use('/', express.static('public'));
-
+app.use('/home', express.static('public'));
+app.use('/about', express.static('public'));
+app.use('/careers', express.static('public'));
 app.listen(process.env.PORT || '3000', () => {
     console.log("server started")
 })
